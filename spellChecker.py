985539a05d09c0ea2cc5eeb8c5a98831ddb93943
Pranav Guruprasad Rao <95999252+PranavRao25@users.noreply.github.com>
2023-07-08 23:07:33 +0530

    Update spellChecker.py

--- spellChecker.py
+++ spellChecker.py
@@ -2,14 +2,18 @@ import re
 
 class SpellChecker: # Spell Checker Class
     text={} # freq table with keys as words, values as freq
+    a,b,g=1,0.9,0.8 # variables for probab calc
     
-    def __init__(self,file='gutenberg.txt'): # creating the dataset
+    def __init__(self,file='gutenberg.txt',a=1,b=0.9,g=0.8): # creating the dataset
+        self.a,self.b,self.g=a,b,g
+        
         with open(file,'r') as f: # text corpus used for valid words
-            for line in f.readlines(): # data formatting
-                for w in re.sub(r'^\W+|\W+$','',line.strip(),re.IGNORECASE).lower().split(' '):
-                    w = re.sub(r'^\W+|\W+$','',w,re.IGNORECASE)
-                    if(w!='' and w.isalpha()): # only lower alphabetic
-                        self.text[w] = (self.text[w]+1) if(w in self.text.keys()) else 0
+            lines = f.readlines()
+        for line in lines: # data formatting
+            for _ in re.sub(r'^\W+|\W+$','',line.strip()).lower().split(' '):
+                w = re.sub(r'^\W+|\W+$','',_)
+                if(w!='' and w.isalpha()): # only lower alphabetic
+                    self.text[w] = (self.text[w]+1) if(w in self.text.keys()) else 0
     
     _valid = lambda self,word: (word in self.text.keys()) # if the given word is valid
     _validW = lambda self,L: list({w for w in L if(self._valid(w))}) # valid words from a list
@@ -28,29 +32,26 @@ class SpellChecker: # Spell Checker Class
     def _insW(self,w): # all words from inserting 1 char
         ins=[]
         for ch in range(ord('a'),ord('z')+1):
-            for i in range(-1,len(w)):
-                ins.append(w[:i+1]+chr(ch)+w[i+1:])
+                ins+=[w[:i+1]+chr(ch)+w[i+1:] for i in range(-1,len(w))]
         return ins
 
     def _repW(self,w): # all words from replacing 1 char
         rep=[]
         for ch in range(ord('a'),ord('z')+1):
-            for i in range(len(w)):
-                rep.append(w[:i]+chr(ch)+w[i+1:])
+                rep+=[w[:i]+chr(ch)+w[i+1:] for i in range(len(w))]
         return rep
 
     # all 1 char variations/misspells of a word
     _edit = lambda self,w: self._delW(w)+self._insW(w)+self._repW(w)+self._swap(w)+self._cycle(w)
 
     def _probab(self,w,c,Fw,Gw): # P(W=w|C=c)
-        a,b=1,0.9
         if(self._valid(w)):
-            return a if(w==c) else 0
+            return self.a if(w==c) else 0
         else:
             if(len(Fw)):
-                return b if(c in Fw) else 0
+                return self.b if(c in Fw) else 0
             else:
-                return g if(c in Gw) else 0
+                return self.g if(c in Gw) else 0
 
     def _correcter(self,w): # Naive Bayesian Classifier
         max,cw,Cw=0,w,self._edit(w)
@@ -73,7 +74,11 @@ class SpellChecker: # Spell Checker Class
             for _ in re.sub(r'^\W+|\W+$','',line.strip()).lower().split(' '):
                 w = re.sub(r'^\W+|\W+$','',_)
                 if(w!='' and w.isalpha() and not self._valid(w)):
-                    print(f'Change {w} -> {self._correcter(w)}')
+                    c=self._correcter(w)
+                    if(c!=w):
+                        print(f'Change {w} -> {c}')
+                    else:
+                        print(f'Unknown word {w}')
 
 spl = SpellChecker() # creating a spellchecker object
 spl.spellCheck()
